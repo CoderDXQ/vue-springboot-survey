b3e73b210081cee4a7fc03ef9f02389f8724cf26
cococolabear <794055465@qq.com>
2020-12-19 22:45:54 +0800

    first submit

---
+++ src/main/java/com/markerhub/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.markerhub.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2020/12/19 10:21 下午
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.markerhub.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
